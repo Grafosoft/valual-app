eeb594ea697950bdc3903013e9a211dbee989c2c
desarrollo junior <desarrollojunior@grafosoft.com>
2023-08-02 16:41:26 -0500

    details #v2

--- layout/items/itemsDetailsHeader.tsx
+++ layout/items/itemsDetailsHeader.tsx
@@ -11,7 +11,7 @@ import {
 } from '@nextui-org/react'
 import { ItemsList } from '../../interfaces/items/itemsList'
 import { MdOutlineInventory2 } from 'react-icons/md'
-import { TbCheck,TbX ,TbHeart,TbHeartFilled  } from 'react-icons/tb'
+import { TbCheck,TbX ,TbHeart,TbHeartFilled ,TbBrandCashapp ,TbReceiptTax } from 'react-icons/tb'
 
 
 
@@ -26,11 +26,17 @@ const currencyFormat = new Intl.NumberFormat('en-DE')
 
   return (
     <div
-      className="grid grid-cols-12 p-5 gap-5"
-      style={{ justifyContent: 'center', minHeight: "92vh" }}
+      className="grid grid-rows-12 p-5 gap-5"
+      style={{ minHeight: "92vh" }}
     >
+    
+      <Card className="row-span-1"></Card>
+  
       
-     
+      <div
+      className="grid grid-cols-12 row-span-11  gap-5"
+      style={{ justifyContent: 'center'}}
+    >
         <Card className="col-span-2 ">
           <CardHeader style={{ padding: '0' }}>
             <div
@@ -44,8 +50,6 @@ const currencyFormat = new Intl.NumberFormat('en-DE')
                 margin: '0'
               }}
             ></div>
-        
-      
 
         <Avatar
               isBordered
@@ -61,16 +65,15 @@ const currencyFormat = new Intl.NumberFormat('en-DE')
                 right: '0',
                 top: '70px'
               }}
-
               src={data.image}
             />
+            
           </CardHeader>
-          <CardBody style={{}}>
 
+          <CardBody style={{}}>
           <Spacer y={20} />
             <div className="flex justify-between w-auto">
-              <h4 className="flex text-xl font-medium">Favorito</h4>
-                
+              <h4 className="flex text-lg font-medium">Favorito</h4>         
               <Switch
                 color="danger"
                 isSelected={data.isFavorite} 
@@ -81,18 +84,13 @@ const currencyFormat = new Intl.NumberFormat('en-DE')
                   <TbHeart  />
                 )
                 }
-            >
-             
-                 
+            >        
                </Switch>
-
-               
-           
                     </div>
+
                     <Spacer y={5} />
-          
                 <div className="flex justify-between w-auto">
-                <h4 className="flex text-xl font-medium pt-2" >
+                <h4 className="flex text-lg font-medium pt-2" >
                   Estado
                 </h4>
                 <Switch isSelected={data.isActive} 
@@ -104,18 +102,13 @@ const currencyFormat = new Intl.NumberFormat('en-DE')
                   <TbX  />
                 )
                 }
-            >
-             
-                 
+            > 
                </Switch>
-
                 </div>
-                <Spacer y={5} />
-              
 
-                
+                <Spacer y={5} />       
                  <div className="flex justify-between w-auto">
-                <h4 className="flex text-xl font-medium pt-2">
+                <h4 className="flex text-lg font-medium pt-2">
                  AIU
                  </h4>
                  <Switch isSelected={data.isAiu} 
@@ -128,14 +121,12 @@ const currencyFormat = new Intl.NumberFormat('en-DE')
                 )
                 }
             >
-             
-                 
                </Switch>
                 </div>
-                <Spacer y={5} />
 
+                <Spacer y={5} />
                  <div className="flex justify-between w-auto">
-                <h4 className="flex text-xl font-medium pt-2">
+                <h4 className="flex text-lg font-medium pt-2">
                  Inventario
                  </h4>
                  <Switch isSelected={data.isInventory} 
@@ -148,49 +139,92 @@ const currencyFormat = new Intl.NumberFormat('en-DE')
                 )
                 }
             >
-             
-                 
                </Switch>
                 </div>
+
                 <Spacer y={5} />
-               
                 <div className="flex justify-between w-auto">
-               
-                 <p className=" flex text-xl font-medium pt-2">
+                 <p className=" flex text-lg font-medium pt-2">
               {data.isInventory ? 'Con control de inventario' : 'Sin control de inventario'}
                </p>
                 </div>
                 <Spacer y={5} />
-              
-
-         
           </CardBody>
-        </Card>
-        <div className="col-span-10 grid grid-rows-12 gap-5">
-        <Card className="row-span-1  "> 
-      
-         
-        </Card>
-        
-        <Card className="row-span-11  ">
-
+          
         </Card>
 
- </div>
-
-
-    
-    </div>
-    
-
-
- 
-    
        
+
+     
+        <Card className="col-span-10 ">
+        <Spacer y={10} />
+        <div       
+              style={{ justifyContent: 'space-between' }}
+            >
+              <div
+              className='text-center'
+              >
+               <h1 className="text-5xl font-bold py-2">{data.name}</h1>
+                <h3 className="text-2xl font-bold pt-2 ">{data.group.name}</h3>
+
+                  </div>
+                  <div  className="grid grid-cols-2 p-5 gap-5">
+               
+                  <Card >   
+                        <div className="flex">
+                    <TbBrandCashapp className=" w-20 h-20 text-default-500"   color="#17c964"/>       
+                 
+                    
+                     <p className="text-2xl font-bold py-7 " style={{ textTransform: 'capitalize' }}>
+                     Precio de costo COP {currencyFormat.format(data.costPrice)}
+                     </p></div>
+                  </Card>
+                  <Card >
+                  <div className=" justify-center items-center p-5">
+                    <TbBrandCashapp className=" w-10 h-10 text-default-500"   color="#17c964"/>     
+                     <p className="text-2xl font-bold " style={{ textTransform: 'capitalize' }}>
+                     Ultimo precio de costo COP {currencyFormat.format(data.lastcostPrice)}
+                    </p></div>
+                  </Card>
+                  <Card >
+                  <div className="flex">
+                    <TbBrandCashapp className=" w-20 h-20 text-default-500"   color="#17c964"/> 
+                    <p className="text-2xl font-bold py-7" style={{ textTransform: 'capitalize' }}>
+                Precio de venta COP {currencyFormat.format(data.salePrice)}
+                </p></div>
+
+                  </Card>
+                  <Card >
+                  <TbReceiptTax className=" w-20 h-20 text-default-500"  size={800} color="#17c964"/>       
+
+                      <p className="font-medium" style={{ textTransform: 'capitalize' }}>
+                      {data.tax.name}
+                      </p>
+                  </Card>
+             
+
+               
+
+               
+                
+                
+              
+              
+               
+                </div>
+
+        
+              
+              </div>
+
               
 
 
-              
+        </Card>
+    </div>
+ 
+    </div>
+                
               
             
   )
