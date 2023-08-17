b11676f94aaca77efa5de27d14e02615ebd9019c
desarrollo junior <desarrollojunior@grafosoft.com>
2023-08-17 15:02:07 -0500

    sidebar finally finished

--- layout/board/DashboardLayout copy.tsx
+++ layout/board/DashboardLayout copy.tsx
@@ -13,7 +13,7 @@ import { NavbarUser } from '../../components/navbar/Navbar'
 
 import { BsBank } from 'react-icons/bs'
 import { MdWarehouse } from 'react-icons/md'
-import Sidebar from '../sidebar/sidebar copy 2'
+import Sidebar, { SidebarItem } from '../sidebar/sidebar copy 2'
 
 interface Props {
   children: ReactNode
@@ -28,24 +28,76 @@ export const DashboardLayout: FC<Props> = ({ children }) => {
  
   return (
   
-      <div className="grid min-h-screen grid-rows-header bg-zinc-100">
-
-        {status === 'authenticated' && <NavbarUser onMenuButtonClick={() => setSidebarOpen((prev) => !prev)}/>}
       
+    <div className="flex">
+    {status === 'authenticated' && (
+        <Sidebar open={sidebarOpen} setOpen={setSidebarOpen}  >
+            <SidebarItem
+            icon={<TbShoppingBag size={25} />}
+            text="Productos"
+            urlPath={`/catalogo`}
+            active={pathname===('/catalogo')}
+            
+          />
+          
+          <SidebarItem
+            icon={<TbUsers size={25} />}
+            text="Clientes"
+            urlPath={`/contactos`}
+            active={pathname === '/contactos'}
+          />
+          <SidebarItem
+            icon={<TbNumber size={25} />}
+            text="Numeracion"
+            urlPath={`/numeracion`}
+            active={pathname === '/numeracion'}
+          />
+           <SidebarItem
+            icon={<MdWarehouse size={25} />}
+            text="Almacences"
+            urlPath={`/almacenes`}
+            active={pathname === '/almacenes'}
+          />
+           <SidebarItem
+            icon={<BsBank size={25} />}
+            text="Bancos"
+            urlPath={`/bancos`}
+            active={pathname === '/bancos'}
+          />
+          <SidebarItem
+            icon={<TbFiles size={25} />}
+            text="Archivos"
+            urlPath={`/archivos`}
+            active={pathname === '/archivos'}
+          />
+           <SidebarItem
+            icon={<TbUserDollar size={25} />}
+            text="Vendedores"
+            urlPath={`/vendedores`}
+            active={pathname === '/vendedores'}
+          />
+           <SidebarItem
+            icon={<TbFileInvoice size={25} />}
+            text="Facturas"
+            urlPath={`/facturas`}
+            active={pathname === '/facturas'}
+          />
+          
+        </Sidebar>
 
-
+        
+         )}
      
-     <div className="  ">
-        <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
-
-
-        {children}
-      
+        
+   
+    
+   
       
 
-    </div>
-     </div>
+    <div className="w-full">
+        {status === 'authenticated' && <NavbarUser onMenuButtonClick={() => setSidebarOpen((prev) => !prev)}/>}
+         {children}
+         </div>
+        </div>
 
-  
-  )
-}
+  )}
